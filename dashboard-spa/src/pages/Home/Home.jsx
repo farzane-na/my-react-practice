e5b8193a2d0ee7deafb119412bf8a29724e8d5a9
farzane nazmabadi <1379farzanenazmabadi@gmail.com>
2024-10-18 03:06:49 +0330

    complete feature component and create data for chart in feature

--- dashboard-spa/src/pages/Home/Home.jsx
+++ dashboard-spa/src/pages/Home/Home.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import "./Home.css"
+import Features from '../../components/Features/Features'
 
 export default function Home(){
-
+    return (
+        <div className="home">
+            <Features />
+        </div>
+    )
 }
