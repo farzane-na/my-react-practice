f1783b62a5ddc1e5b0b4ae18f0cfba14b625e133
farzane nazmabadi <1379farzanenazmabadi@gmail.com>
2024-11-03 17:18:18 +0330

    style to inputs

--- dashboard-spa/src/pages/NewProduct/NewProduct.jsx
+++ dashboard-spa/src/pages/NewProduct/NewProduct.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./NewProduct.css"
-
+import {category} from "./../../data/product"
 
 export default function NewProduct() {
   return (
@@ -9,20 +9,36 @@ export default function NewProduct() {
         <form action="" className='new-product__form'>
             <div className="input__wrapper">
             <div className="new-product__box">
+                <p>
+                <input type="text" className="new-product__input" />
                 <label htmlFor="" className="new-product__label">نام محصول</label>
-                <input type="text" className="new-product__input" />
+                </p>
             </div>
             <div className="new-product__box">
-                <label htmlFor="" className="new-product__label"></label>
+                <p>
+
                 <input type="text" className="new-product__input" />
+                <label htmlFor="" className="new-product__label">قیمت</label>
+                </p>
+            </div>
+            <div className="new-product__box" >
+                {/* <label htmlFor="" className="new-product__label"></label>
+                <input type="text" className="new-product__input" /> */}
+                <select name="" id="" className="new-product__select">
+                    <option value="" selected>لطفا یک دسته بندی را انتخاب کنید ...</option>
+                    {
+                        category.map(cat=>(
+                            <option>{cat.title}</option>
+                        ))
+                    }
+                </select>
             </div>
             <div className="new-product__box">
-                <label htmlFor="" className="new-product__label"></label>
-                <input type="text" className="new-product__input" />
-            </div>
-            <div className="new-product__box">
-                <label htmlFor="" className="new-product__label"></label>
-                <input type="text" className="new-product__input" />
+                <p>
+
+                <textarea rows={10} className="new-product__input "> </textarea>
+                <label htmlFor="" className="new-product__label">توضیحات محصول</label>
+                </p>
             </div>
 
             </div>
