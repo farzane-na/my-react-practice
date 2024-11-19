3d76914312fabc2f09b246686d3fc0b7ae70e147
farzane nazmabadi <1379farzanenazmabadi@gmail.com>
2024-11-19 21:00:34 +0330

    create restaurant menu profjrct

---
+++ restaurant-menu/src/data/data.js
@@ -0,0 +1,99 @@
+const foods=[
+    {
+        id:1,
+        name:"Hotcakes and Sausage",
+        description:"When you're craving pancakes and sausage, McDonald's has you covered with our Hotcakes and Sausage breakfast.",
+        category:"breakfast",
+        cal:"770",
+        image:"https://s7d1.scene7.com/is/image/mcdonaldsstage/DC_202405_0032_3HotCakes_Sausage_1564x1564?wid=1000&hei=1000&dpr=off"
+    },
+    {
+        id:2,
+        name:"Big Breakfast® with Hotcakes",
+        description:"McDonald’s Big Breakfast® with Hotcakes satisfies with both sweet and savory breakfast favorites.",
+        category:"breakfast",
+        cal:"1340",
+        image:"https://s7d1.scene7.com/is/image/mcdonaldsstage/DC_202208_3590_BigBreakfast_HotCakes_1564x1564?wid=1000&hei=1000&dpr=off"
+    },
+    {
+        id:3,
+        name:"Egg McMuffin® Meal",
+        description:"Get more for breakfast with the McDonald’s Egg McMuffin® Meal. ",
+        category:"breakfast",
+        cal:"455",
+        image:"https://s7d1.scene7.com/is/image/mcdonaldsstage/DC_202007_0252_EVM_HB_EggMcMuffin_Coffee_Glass_1564x1564"
+    },
+    {
+        id:4,
+        name:"Double Quarter Pounder®* with Cheese",
+        description:"Each Double Quarter Pounder with Cheese features two quarter pound* 100% fresh beef burger patties that are hot, deliciously juicy and cooked when you order. ",
+        category:"burger",
+        cal:"740",
+        image:"https://s7d1.scene7.com/is/image/mcdonalds/DC_202201_3426-005_DoubleQuarterPounderwithCheese_1564x1564-1?wid=1000&hei=1000&dpr=off"
+    },
+    {
+        id:5,
+        name:"Quarter Pounder®* with Cheese Deluxe",
+        description:"McDonald's Quarter Pounder® with Cheese Deluxe is a fresh take on a Quarter Pounder® classic burger.",
+        category:"burger",
+        cal:"630",
+        image:"https://s7d1.scene7.com/is/image/mcdonaldsstage/DC_202309_4282_QuarterPounderCheeseDeluxe_Shredded_1564x1564?wid=1000&hei=1000&dpr=off"
+    },
+    {
+        id:6,
+        name:"Double Cheeseburger",
+        description:"The McDonald's Double Cheeseburger features two 100% pure all beef patties seasoned with just a pinch of salt and pepper.",
+        category:"burger",
+        cal:"440",
+        image:"https://s7d1.scene7.com/is/image/mcdonaldsstage/DC_202302_0004-999_DoubleCheeseburger_Alt_1564x1564?wid=1000&hei=1000&dpr=off"
+    },
+    {
+        id:7,
+        name:"McCafé® Caramel Macchiato",
+        description:"Ever wondered what's in a McCafé Caramel Macchiato from McDonald's? Caramel Macchiato combines bold, rich espresso and buttery caramel flavor served with steamed whole milk.",
+        category:"coffee",
+        cal:"260",
+        image:"https://s7d1.scene7.com/is/image/mcdonaldsstage/DC_201906_2804_MediumCaramelMacchiato_Glass_A1_HL_1564x1564?wid=1000&hei=1000&dpr=off"
+    },
+    {
+        id:8,
+        name:"McCafé® Mocha Latte",
+        description:"Enjoy a delicious, hot Mocha Latte from McDonald’s. Wondering what’s in a Mocha Latte?",
+        category:"coffee",
+        cal:"290",
+        image:"https://s7d1.scene7.com/is/image/mcdonalds/DC_201906_2731_MediumMocha_Glass_A1_HL_1564x1564-1?wid=1000&hei=1000&dpr=off"
+    },
+    {
+        id:9,
+        name:"McCafé® Iced Caramel Macchiato",
+        description:"McCafé Iced Caramel Macchiato is the chilled equivalent of the McCafé Caramel Macchiato.",
+        category:"coffee",
+        cal:"200",
+        image:"https://s7d1.scene7.com/is/image/mcdonalds/DC_201906_2743_MediumIcedCaramelMacchiato_Glass_A1_1564x1564-1?wid=1000&hei=1000&dpr=off"
+    },
+    {
+        id:10,
+        name:"Coca-Cola®",
+        description:"Enjoy a cold, refreshing Coca-Cola® soda from McDonald's that complements all your menu favorites.",
+        category:"drink",
+        cal:"200",
+        image:"https://s7d1.scene7.com/is/image/mcdonalds/DC_202402_0521_MediumCoke_ContourGlassv1_1564x1564?wid=1000&hei=1000&dpr=off"
+    },
+    {
+        id:11,
+        name:"Strawberry Banana Smoothie",
+        description:"The McCafé® Strawberry Banana Smoothie recipe features the perfect combination of real strawberry and banana fruit purees and juices, blended with creamy low fat yogurt and ice.",
+        category:"drink",
+        cal:"190",
+        image:"https://s7d1.scene7.com/is/image/mcdonaldsstage/DC_201906_2725_MediumStrawberryBananaSmoothie_Glass_A1_1564x1564?wid=1000&hei=1000&dpr=off"
+    },
+    {
+        id:12,
+        name:"Fanta® Orange",
+        description:"Sip on the bubbly, refreshing orange flavor of Fanta® Orange soda from McDonald’s! Is McDonald’s Fanta® Orange caffeine-free? It sure is, making it a perfect addition to any of your favorite McDonald's items.",
+        category:"drink",
+        cal:"210",
+        image:"https://s7d1.scene7.com/is/image/mcdonalds/DC_202212_1262_MediumFantaOrange_Glass_1564x1564-1?wid=1000&hei=1000&dpr=off"
+    },
+]
+export default foods
