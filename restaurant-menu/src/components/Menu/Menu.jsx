f84223008618843a43c1bd2263bef271e3d0972f
farzane nazmabadi <1379farzanenazmabadi@gmail.com>
2024-11-21 11:25:38 +0330

    filter card with clickhing on manu item

--- restaurant-menu/src/components/Menu/Menu.jsx
+++ restaurant-menu/src/components/Menu/Menu.jsx
@@ -6,6 +6,8 @@ import "./Menu.css";
 export default function Menu() {
     const [data,setData]=useState(foods)
     const [menuItem,setMenuItem]=useState([])
+    const [activeCategory,setActiveCategory]=useState("all")
+    const [filteredFood,setFilteredFood]=useState(foods)
     useEffect(()=>{
         const filteredCategory=[]
         data.forEach(item=>{
@@ -13,22 +15,37 @@ export default function Menu() {
         })
         console.log(filteredCategory);
         const setFoodCategory=new Set(filteredCategory)
-        setMenuItem([...setFoodCategory])
+        setMenuItem(["all",...setFoodCategory])
+        console.log(filteredFood);
+        
     },[])
-    
+    const chnageCategory=(event)=>{
+        console.log(event.target.getAttribute("name"));
+        setActiveCategory(event.target.getAttribute("name"))
+    }
+    useEffect(()=>{
+        const filterMenu=data.filter(item=>{
+            return item.category===activeCategory
+        })
+        if(activeCategory==="all"){
+            setFilteredFood(data)
+        }else{
+
+            setFilteredFood(filterMenu)
+        }
+    },[activeCategory])
   return (
     <div className="menu">
       <ul className="menu-list">
-      <li className="menu-list__item active">all</li>
         {
             menuItem.map(item=>{
-                return <li key={item} className="menu-list__item">{item}</li>
+                return <li key={item} className={`menu-list__item`} name={item} onClick={(event)=>chnageCategory(event)}>{item}</li>
             })
         }
       </ul>
       <div className="card-wrapper">
         {
-            data.map(food=>{
+            filteredFood.map(food=>{
                 return <Card key={food.id} {...food} />
             })
         }
