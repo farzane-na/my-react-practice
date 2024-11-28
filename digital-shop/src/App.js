27f87c5decc13920b5e873cf3b844ff429c81786
farzane nazmabadi <1379farzanenazmabadi@gmail.com>
2024-11-28 04:03:42 +0330

    start digital shop project

---
+++ digital-shop/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React, { useState } from "react";
+
+function App() {
+  const [firstCount, setFirstCount] = useState(0);
+  
+  
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
