70c79278de89fa8dac03769a3aa9ae28444a977d
farzane nazmabadi <1379farzanenazmabadi@gmail.com>
2024-12-01 21:48:59 +0330

    adding slider

--- digital-shop/src/App.js
+++ digital-shop/src/App.js
@@ -5,13 +5,10 @@ import Header from "./components/Header/Header";
 
 function App() {
   const [firstCount, setFirstCount] = useState(0);
-  
-  
+
   return (
     <div className="App pt-2 sm:pt-4 lg:pt-5">
-      <div className="wrapper mx-auto px-3 sm:px-5 lg:px-8 w-full">
-        <Header />
-      </div>
+      <Header />
     </div>
   );
 }
--- digital-shop/src/components/Header/Header.jsx
+++ digital-shop/src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import NavBar from '../NavBar/NavBar'
+import React from "react";
+import NavBar from "../NavBar/NavBar";
+import Slider from "../Slider/Slider";
 
 export default function Header() {
   return (
     <header>
+      <div className="wrapper">
         <NavBar />
+      </div>
+      <Slider />
     </header>
-  )
+  );
 }
