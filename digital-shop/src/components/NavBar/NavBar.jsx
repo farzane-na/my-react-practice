c6470f39f72ec1c231e21091505a40ea2158930e
farzane nazmabadi <1379farzanenazmabadi@gmail.com>
2024-12-01 10:43:02 +0330

    responsive navbar

--- digital-shop/src/components/NavBar/NavBar.jsx
+++ digital-shop/src/components/NavBar/NavBar.jsx
@@ -1,40 +1,77 @@
 import React from "react";
 import logo from "./../../asset/logo/static_logo_techno_new.svg";
-import { CiShoppingCart, CiSearch } from "react-icons/ci";
+import { CiShoppingCart, CiSearch, CiMenuBurger } from "react-icons/ci";
 
 export default function NavBar() {
   return (
-    <div className="navbar">
-      <div className="w-full flex justify-between items-center">
-        <div className="flex gap-x-8">
+    <>
+      <div className="navbar hidden md:block">
+        <div className="w-full flex justify-between items-center">
+          <div className="flex gap-x-8">
+            <div className="logo flex justify-center items-center">
+              <img src={logo} alt="تکنولایف" />
+            </div>
+            <div className="searchbar flex items-center gap-x-5 p-4 bg-slate-100 rounded-md w-[450px]">
+              <CiSearch className="text-slate-500 cursor-pointer w-6 h-6" />
+              <input
+                type="text"
+                className="bg-transparent border-none outline-none text-slate-500 w-full"
+                placeholder="محصول، برند یا دسته مورد نظرتان را جستجو کنید"
+              />
+            </div>
+          </div>
+          <div className="flex items-center gap-x-10">
+            <div className="login-btn rounded-md border border-slate-950  py-2 flex items-center">
+              <div className="border-l border-slate-950 px-5">
+                <a href="#">ورود</a>
+              </div>
+              <div className="px-5">
+                <a href="#">ثبت نام</a>
+              </div>
+            </div>
+            <div className="cart border border-slate-100 rounded-md p-2">
+              <CiShoppingCart className="text-slate-950 w-6 h-6 cursor-pointer" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="navbar-mobile flex md:hidden flex-col gap-y-7">
+        <div className="topbar flex justify-between items-center">
+          <CiMenuBurger className="text-slate-950 w-6 h-6 cursor-pointer" />
           <div className="logo flex justify-center items-center">
             <img src={logo} alt="تکنولایف" />
           </div>
-          <div className="searchbar flex items-center gap-x-5 p-4 bg-slate-200 rounded-md w-[450px]">
-            <CiSearch
-              className="text-slate-500 cursor-pointer w-6 h-6"
-            />
+          <div className="login flex items-center gap-x-2 text-slate-950">
+            <span>ورود</span>
+            <svg
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="rotate-180 text-slate-950 w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25"
+              />
+            </svg>
+          </div>
+        </div>
+        <div className="flex justify-between items-center">
+          <div className="searchbar flex items-center gap-x-5 p-4 bg-slate-100 rounded-md w-[85%]">
+            <CiSearch className="text-slate-500 cursor-pointer w-6 h-6" />
             <input
               type="text"
               className="bg-transparent border-none outline-none text-slate-500 w-full"
               placeholder="محصول، برند یا دسته مورد نظرتان را جستجو کنید"
             />
           </div>
-        </div>
-        <div className="flex items-center gap-x-10">
-          <div className="login-btn rounded-md border border-slate-950  py-2 flex items-center">
-            <div className="border-l border-slate-950 px-5">
-              <a href="#">ورود</a>
-            </div>
-            <div className="px-5">
-              <a href="#">ثبت نام</a>
-            </div>
-          </div>
-          <div className="cart border border-slate-300 rounded-md p-2">
+          <div className="cart p-2">
             <CiShoppingCart className="text-slate-950 w-6 h-6 cursor-pointer" />
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
