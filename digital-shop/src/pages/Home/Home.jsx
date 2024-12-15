89505f1af722f1af8ef552fe76e1f826d77d6542
farzane nazmabadi <1379farzanenazmabadi@gmail.com>
2024-12-15 16:16:20 +0330

    adding some more section to home page

--- digital-shop/src/pages/Home/Home.jsx
+++ digital-shop/src/pages/Home/Home.jsx
@@ -8,7 +8,16 @@ import banner1 from "./../../asset/banner/banner-in-home/1.webp"
 import banner2 from "./../../asset/banner/banner-in-home/2.webp"
 import banner3 from "./../../asset/banner/banner-in-home/3.webp"
 
-const images=[banner1,banner2,banner3]
+import mobile1 from "./../../asset/banner/mobile-picture/1.webp"
+import mobile2 from "./../../asset/banner/mobile-picture/2.webp"
+import mobile3 from "./../../asset/banner/mobile-picture/3.webp"
+import mobile4 from "./../../asset/banner/mobile-picture/4.webp"
+import mobile5 from "./../../asset/banner/mobile-picture/5.webp"
+
+import ban from "./../../asset/banner/ban.webp"
+
+const bannerImages=[banner1,banner2,banner3]
+const mobileImages=[mobile1,mobile2,mobile3,mobile4,mobile5]
 
 export default function Home() {
   return (
@@ -20,7 +29,7 @@ export default function Home() {
 
             <div className="flex flex-col md:flex-row items-center gap-4 mb-10">
               {
-                images.map((img,index)=>{
+                bannerImages.map((img,index)=>{
                   return (
                     <div className="relative rounded-xl overflow-hidden" key={index}>
                       <img src={img} alt="" className='w-full h-full'/>
@@ -32,6 +41,28 @@ export default function Home() {
             </div>
 
             <ProductSlider off={false} category={"mobile"} border={false} title={"پرچمداران هوشمند"} />
+
+            <div className="flex flex-col gap-4 mb-10">
+              <h2 className='font-shabnamBold text-2xl text-center'>برترین های موبایل</h2>
+              <div className="flex flex-col md:flex-row justify-center items-center gap-3">
+                {
+                  mobileImages.map((img,index)=>{
+                    return (
+                      <div className="relative" key={index}>
+                        <img src={img} alt="" className='w-full h-full'/>
+                        <Link href="#" className='absolute w-full h-full top-0 bottom-0 left-0 right-0 z-20'></Link>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+            </div>
+
+            <div className="w-full mb-10 rounded-3xl overflow-hidden">
+              <img src={ban} alt="" className='w-full' />
+            </div>
+
+            <ProductSlider off={false} category={"laptop"} border={false} title={"لپ‌تاپ‌ها در تکنولایف"} />
         </div>
     </div>
   )
